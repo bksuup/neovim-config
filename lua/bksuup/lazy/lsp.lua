8f20d19f9781310e2160715fbdad681401f158a8
Bjørn Kristian Strand <bjorkst@stud.ntnu.no>
2025-01-19 21:58:34 +0100

    feat: re-write lsp.lua conf

--- lua/bksuup/lazy/lsp.lua
+++ lua/bksuup/lazy/lsp.lua
@@ -1,94 +1,61 @@
---[ Mostly stolen from https://github.com/mathias-ws/nvim and https://github.com/ThePrimeagen/neovimrc --]
-
 return {
-    "VonHeikemen/lsp-zero.nvim",
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- lazydev is only for editing the nvim config files and is optional af
+			{
+				"folke/lazydev.nvim",
+				ft = "lua",
+				opts = {
+					library = {
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "luvit-meta/library", words = { "vim%.uv" } },
+					},
+				},
+			},
 
-    dependencies = {
-        { "neovim/nvim-lspconfig" }, -- Required
-        { "williamboman/mason.nvim" },
-        { "williamboman/mason-lspconfig.nvim" }, -- Optional
-        { "WhoIsSethDaniel/mason-tool-installer.nvim" },
-        -- Autocompletion
-        { "hrsh7th/nvim-cmp" }, -- Required
-        { "hrsh7th/cmp-nvim-lsp" }, -- Required
-        { "L3MON4D3/LuaSnip" }, -- Required
-    },
+			-- Installing and configuring LSP-servers
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-    config = function()
+			-- autoformating
+			"stevearc/conform.nvim",
+		},
 
-        require("mason").setup()
+		config = function()
+			local mason_tool_installer = require("mason-tool-installer")
+			local mason_lspconfig = require("mason-lspconfig")
+			local lspconfig = require("lspconfig")
 
-        local lsp = require("lsp-zero")
-        local mason_tool_installer = require("mason-tool-installer")
-        local mason_lspconfig = require("mason-lspconfig")
+			-- call mason setup function
+			require("mason").setup()
 
-        lsp.on_attach(function(client, bufnr)
-            local opts = {buffer = bufnr, remap = false}
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        end)
+			-- Ensure wanted lsp's are installed
+			mason_lspconfig.setup({
+				automatic_installation = true,
+				ensure_installed = {
+					"lua_ls",
+				},
+			})
 
-        lsp.omnifunc.setup({
-            tabcomplete = true,
-            use_fallback = true,
-            update_on_delete = true,
-        })
+			-- ensure wanted tools are installed
+			mason_tool_installer.setup({
+				ensure_installed = {
+					"stylua",
+				},
+			})
 
+			lspconfig.lua_ls.setup({})
 
-        mason_lspconfig.setup({
-            ensure_installed = {
-                "lua_ls",
-                "perlnavigator",
-                "pyright",
-                "yamlls",
-                "bashls",
-                "ansiblels",
-                "gopls",
-                "terraformls",
-            }
-        })
-
-
-        mason_tool_installer.setup({
-            ensure_installed = {
-                "yamllint",
-                "ruff",
-                "mypy",
-                "ansible-lint",
-            }
-        })
-
-        require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
-        --[ require("lspconfig").lua_ls.setup({}) --]
-
-        require("lspconfig").perlnavigator.setup({})
-        require("lspconfig").bashls.setup({})
-        require("lspconfig").ansiblels.setup({})
-        require("lspconfig").gopls.setup({})
-        require("lspconfig").terraformls.setup({})
-
-        require("lspconfig").pyright.setup({
-            filetypes = { "python" },
-        })
-
-        require("lspconfig").yamlls.setup({
-            settings = {
-                yaml = {
-                    format = {
-                        enable = false,
-                    },
-                    validate = true,
-                    completion = true,
-                    hover = true,
-                },
-            },
-        })
-
-    end
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+				},
+				format_on_save = {
+					timeout_ms = 500,
+				},
+			})
+		end,
+	},
 }
--- lua/bksuup/set.lua
+++ lua/bksuup/set.lua
@@ -14,4 +14,4 @@ vim.opt.scrolloff = 8
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "80"
